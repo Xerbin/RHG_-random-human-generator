89e3f3e48860ff80be052382528d089b57c5ecc4
LimoMaster <140161944+LimoMaster@users.noreply.github.com>
2024-07-08 12:11:57 -0700

    Add files via upload
    it's a beta version and doesn't have any comment. also, it's incomplete.

--- random human generator.py
+++ random human generator.py
@@ -9,16 +9,24 @@ else:
 ADBOOL = input("do you want address?")
 if ADBOOL == 'yes':
     address = faker.address()
+else:
+    address = None
 EMBOOL = input("do you want email?(with google domain)")
 if EMBOOL == 'yes':
     email = faker.email(True, "google.com")
+else:
+    email = None
 CCBOOL = input("do you want Credit card address?(visa type only)")
 if CCBOOL == 'yes':
     Credit = faker.credit_card_number('visa')
+else:
+    Credit = None
 BDBOOL = input("do you want Birth Date?")
 if BDBOOL == 'yes':
     MINAGE = int(input("Enter minimum age (def = 0)"))
     MAXAGE = int(input("Enter maximum age (def = 115)"))
     birthday = faker.date_of_birth(None, MINAGE, MAXAGE)
+else:
+    birthday = None
 print("here's you data!")
 print("Name", Name, "Address", address, "Email", email, "Credit Card", Credit, "Birth Date", birthday)
