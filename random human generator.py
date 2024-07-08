a7f0eebe924feb3b05ec1ae4b5ca49b23ede2620
LimoMaster <140161944+LimoMaster@users.noreply.github.com>
2024-07-08 11:57:15 -0700

    Add files via upload

---
+++ random human generator.py
@@ -0,0 +1,24 @@
+from faker import Faker
+
+faker = Faker()
+Name = input("enter RND for random name(enter anything else for custom name): ")
+if Name == 'RND':
+    Name = faker.name()
+else:
+    Name = input("Enter Human name")
+ADBOOL = input("do you want address?")
+if ADBOOL == 'yes':
+    address = faker.address()
+EMBOOL = input("do you want email?(with google domain)")
+if EMBOOL == 'yes':
+    email = faker.email(True, "google.com")
+CCBOOL = input("do you want Credit card address?(visa type only)")
+if CCBOOL == 'yes':
+    Credit = faker.credit_card_number('visa')
+BDBOOL = input("do you want Birth Date?")
+if BDBOOL == 'yes':
+    MINAGE = int(input("Enter minimum age (def = 0)"))
+    MAXAGE = int(input("Enter maximum age (def = 115)"))
+    birthday = faker.date_of_birth(None, MINAGE, MAXAGE)
+print("here's you data!")
+print("Name", Name, "Address", address, "Email", email, "Credit Card", Credit, "Birth Date", birthday)
