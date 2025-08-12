10c66b57f2e8f8a73336680d3af12c65cefcfe67
Milad <140161944+Xerbin@users.noreply.github.com>
2025-08-12 12:39:05 +0330

    Update random human generator.py

--- random human generator.py
+++ random human generator.py
@@ -1,32 +1,35 @@
 from faker import Faker
 
 faker = Faker()
-Name = input("enter RND for random name(enter anything else for custom name): ")
+Name = input("enter RND for random name(or enter your custom name): ")
 if Name == 'RND':
     Name = faker.name()
-else:
-    Name = input("Enter Human name")
-ADBOOL = input("do you want address?")
-if ADBOOL == 'yes':
+#ADBOOL = input("do you want an address?")
+#if ADBOOL == 'yes':
     address = faker.address()
-else:
-    address = None
-EMBOOL = input("do you want email?(with google domain)")
-if EMBOOL == 'yes':
-    email = faker.email(True, "google.com")
-else:
-    email = None
-CCBOOL = input("do you want Credit card address?(visa type only)")
-if CCBOOL == 'yes':
-    Credit = faker.credit_card_number('visa')
-else:
-    Credit = None
-BDBOOL = input("do you want Birth Date?")
-if BDBOOL == 'yes':
-    MINAGE = int(input("Enter minimum age (def = 0)"))
-    MAXAGE = int(input("Enter maximum age (def = 115)"))
-    birthday = faker.date_of_birth(None, MINAGE, MAXAGE)
-else:
-    birthday = None
+#else:
+#    address = None
+#EMBOOL = input("do you want an email?")
+#if EMBOOL == 'yes':
+email = faker.email(True, "google.com")
+#else:
+#    email = None
+#CCBOOL = input("do you want the visa credit card info?")
+#if CCBOOL == 'yes':
+Credit = faker.credit_card_number('visa')
+#else:
+#    Credit = None
+#BDBOOL = input("do you want Birth Date?")
+#if BDBOOL == 'yes':
+MINAGE = int(input("Enter minimum age (def = 0)"))
+MAXAGE = int(input("Enter maximum age (def = 115)"))
+birthday = faker.date_of_birth(None, MINAGE, MAXAGE)
+#else:
+#    birthday = None
 print("here's you data!")
-print("Name", Name, "Address", address, "Email", email, "Credit Card", Credit, "Birth Date", birthday)
+print("Name: ", Name) 
+print("Address: ", address)
+print("Email: ", email)
+print("Credit Card info: ", Credit)
+print("Birth Date: ", birthday)
+
